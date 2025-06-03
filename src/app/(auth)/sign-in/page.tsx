95f54246ac55af0b211ae050da50fd1593860890
vansh kabra <vanshkabra05@gmail.com>
2025-06-03 20:14:31 +0530

    auth setup

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import { SignInView } from "@/app/module/auth/ui/view/signin-view";
+
+const page = ()=>{
+    return (
+        <SignInView/>
+    )
+}
+export default page;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import { SignUpView } from "@/app/module/auth/ui/view/signup-view";
+
+const page = ()=>{
+    return (
+        <SignUpView/>
+    )
+}
+export default page;
