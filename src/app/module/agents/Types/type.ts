0d39c3e938804d372715f014d31a00c775497a7f
VANSH3104 <vanshkabra05@gmail.com>
2025-10-29 16:34:02 +0530

    adding new agent page better planned and optimized

--- src/app/module/agents/Types/type.ts
+++
@@ -1,5 +0,0 @@
-import { inferRouterOutputs } from "@trpc/server";
-import type { AppRouter } from "@/trpc/routers/_app";
-export type WorkflowGetOne = inferRouterOutputs<AppRouter>["workflow"]["getOne"];
-export type WorkflowGetMany = inferRouterOutputs<AppRouter>["workflow"]["getMany"];
-export type 
